279e739bd8d9cb45b1c7b84639b09a50a00ca65c
Anton Kalyutich <anton.kalyutich@gmail.com>
2020-06-16 21:17:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var musiciansPlay = [];
   for (var i = 0; i < musicians.length; i++) {
-    musiciansPlay.push(`m${usicians[i]} plays ${instruments[i]});
+    musiciansPlay.push(`m${usicians[i]} plays ${instruments[i]}`);
   }
   return musiciansPlay;
 }
